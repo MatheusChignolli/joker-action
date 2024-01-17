ad9f963ab6c849d2ceffefafde67adcefcda44d2
Matheus Chignolli <matheuschignolli@gmail.com>
2024-01-17 08:47:19 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # joker-action
-# joker-action
---
+++ main.js
@@ -0,0 +1,25 @@
+const core = require("@actions/core");
+const request = require("request-promise");
+
+const options = {
+  method: "GET",
+  uri: "https://icanhazdadjoke.com/",
+  headers: {
+    Accept: "application/json",
+    "User-Agent": "Writing JavaScript action GitHub Skills course.",
+  },
+  json: true,
+};
+
+async function getJoke() {
+  const res = await request(options);
+  return res.joke;
+}
+
+async function run() {
+  const joke = await getJoke();
+  console.log(joke);
+  core.setOutput("joke-output", joke);
+}
+
+run();
